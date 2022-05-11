7f0e063ab3ed7f4bb41465da34fc65a3e55d5c16
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-11 15:02:11 -0400

    This file will contain the composable UI screen for login.

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoScreen.kt
@@ -0,0 +1,16 @@
+package com.zybooks.foodamy.presentation.login_info
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginInfoScreen(
+    email: String,
+    password: String,
+    //viewModel: LoginInfoViewModel = hiltViewModel()
+){
+//    val state = viewModel.state
+//    if(state.error == null){}
+
+}
+
+
