06c977f5ecc80bde1584809c63b54b7728ca38b6
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-11 15:01:28 -0400

    LoginInfoViewModel will take the users email and password to make the api call and will return a state base of whether it was successful or not.

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoViewModel.kt
@@ -0,0 +1,53 @@
+package com.zybooks.foodamy.presentation.login_info
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zybooks.foodamy.domain.repository.LoginRepository
+import com.zybooks.foodamy.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginInfoViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val repository: LoginRepository
+) : ViewModel() {
+
+    var state by mutableStateOf(LoginInfoState())
+
+    init {
+        viewModelScope.launch {
+            val email = savedStateHandle.get<String>("email") ?: return@launch
+            val password = savedStateHandle.get<String>("password") ?: return@launch
+            state = state.copy(isLoading = true)
+            val loginInfoResult = async { repository.postLoginInfo(email, password)}
+            when(val result = loginInfoResult.await()){
+                is Resource.Success -> {
+                    state = state.copy(
+                        login = result.data,
+                        isLoading = false,
+                        error = null
+                    )
+
+                }
+                is Resource.Error -> {
+                    state = state.copy(
+                        isLoading = false,
+                        error = result.message,
+                        login = null
+                    )
+
+                }
+                else -> Unit
+            }
+
+
+        }
+    }
+}
