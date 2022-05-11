a30ef05a769bcc532df5d20791062f7c85f73dd7
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-11 14:31:49 -0400

    Created the Post Request for retrofit to begin Login Network Call

---
+++ app/src/main/java/com/zybooks/foodamy/data/remote/LoginApi.kt
@@ -0,0 +1,21 @@
+package com.zybooks.foodamy.data.remote
+
+import com.zybooks.foodamy.data.remote.response.LoginResponse
+import okhttp3.ResponseBody
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface LoginApi {
+
+    @FormUrlEncoded
+    @POST("api/auth/login")
+    suspend fun postLogin(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
+
+
+
+
+}
