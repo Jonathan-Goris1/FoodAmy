ae73e8a5c92494eb341edc9f7d33cb8a7af8caa3
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-13 14:56:50 -0400

    Renamed from LoginResponse.kt to AuthResponse because both login and register have the same response. I merged both call to a single data class

---
+++ app/src/main/java/com/zybooks/foodamy/data/remote/AuthApi.kt
@@ -0,0 +1,32 @@
+package com.zybooks.foodamy.data.remote
+
+import com.zybooks.foodamy.data.remote.response.auth.AuthResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @FormUrlEncoded
+    @POST("api/auth/login")
+    suspend fun postLogin(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): AuthResponse
+
+    @FormUrlEncoded
+    @POST("api/auth/register")
+    suspend fun postRegister(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("username") username: String,
+        @Field("name") name: String,
+        @Field("surname") surname: String,
+        @Field("gender") gender: String?,
+        @Field("birthday") birthday: String?
+    ): AuthResponse
+
+
+
+
+}
