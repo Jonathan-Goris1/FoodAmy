998dc74af1fed65cbc69dd69ac0ff4446c59ea96
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-13 14:57:40 -0400

    Renamed from LoginRepository.kt.kt to AuthRepository because both login and register have the same response. I merged both calls to a single repository

---
+++ app/src/main/java/com/zybooks/foodamy/domain/repository/AuthRepository.kt
@@ -0,0 +1,22 @@
+package com.zybooks.foodamy.domain.repository
+
+import com.zybooks.foodamy.data.remote.response.auth.AuthResponse
+import com.zybooks.foodamy.util.Resource
+
+interface AuthRepository {
+
+    suspend fun postLoginInfo(
+        email: String,
+        password: String
+    ): Resource<AuthResponse>
+
+    suspend fun postRegisterInfo(
+        email: String,
+        password: String,
+        username: String,
+        name: String,
+        surname: String,
+        gender: String?,
+        birthday: String?
+    ): Resource<AuthResponse>
+}
