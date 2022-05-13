59e2f413cd5596fee1b6d7599f6e5013d04c0bd3
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-13 15:02:08 -0400

    Imports were changed when changing the class names for responses

--- app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoState.kt
@@ -1,9 +1,9 @@
 package com.zybooks.foodamy.presentation.login_info
 
-import com.zybooks.foodamy.data.remote.response.LoginResponse
+import com.zybooks.foodamy.data.remote.response.auth.AuthResponse
 
 data class LoginInfoState(
-    val login: LoginResponse? = null,
+    val login: AuthResponse? = null,
     val isLoading: Boolean = false,
     val error: String? = null
 )
