233a3a668f1cdd397839fdc5a4d92209b12b437c
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-17 16:45:51 -0400

    Created a module for the repository

---
+++ app/src/main/java/com/zybooks/foodamy/di/RepositoruModule.kt
@@ -0,0 +1,19 @@
+package com.zybooks.foodamy.di
+
+import com.zybooks.foodamy.data.repository.AuthRepositoryImpl
+import com.zybooks.foodamy.domain.repository.AuthRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}
