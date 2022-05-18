01038185015fcb247d39c5e985bcce39089bf68a
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-18 16:33:50 -0400

    Continuing to work on the UI, Will update with proper comments once i begin to finalize what UI I want

--- app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoScreen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoScreen.kt
@@ -1,9 +1,8 @@
 package com.zybooks.foodamy.presentation.login_info
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -14,13 +13,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-@Preview
+
 @Composable
 fun LoginInfoScreen(
 
@@ -71,6 +77,49 @@ fun LoginInfoScreen(
         EmailTextField()
         PasswordTextField()
 
+        Row(
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+            ClickableText(
+                modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp ),
+                text = AnnotatedString("Sign Up"),
+                style = TextStyle(Color.Black, fontWeight = Bold, textAlign = TextAlign.Left),
+                onClick = { offset ->
+                    Log.d("ClickableText", "$offset -th character is clicked.")
+                }
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            ClickableText(
+                modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp ),
+                text = AnnotatedString("Forgot Password"),
+                style = TextStyle(Color.Black, fontWeight = Bold, textAlign = TextAlign.Right),
+                onClick = { offset ->
+                    Log.d("ClickableText", "$offset -th character is clicked.")
+                }
+            )
+
+        }
+
+        Button(
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 0.dp, vertical = 16.dp),
+
+            onClick = {},
+
+            enabled = true,
+
+            shape = MaterialTheme.shapes.medium,
+
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green)
+        ) {
+            Text(text = "Login", color = Color.White)
+        }
+
         
 
 
@@ -96,14 +145,16 @@ fun DividerText() {
         )
 
         Text(
-            modifier = Modifier.padding(0.dp, 0.dp, 16.dp, 0.dp),
-            text = "This is a test",
+
+            maxLines = 1,
+            text = "This is a testtttttttttttttt",
             color = Color.White
         )
 
         Divider(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(16.dp, 0.dp, 0.dp, 0.dp),
 
             color = Color.White,
             thickness = 1.dp
@@ -116,10 +167,11 @@ fun EmailTextField(){
     var value by remember { mutableStateOf("") }
 
     TextField(
+        textStyle = TextStyle(textAlign = TextAlign.Left),
         modifier = Modifier.fillMaxWidth(),
         value = value,
         onValueChange = {value = it },
-        label = { Text(text = "Email or Username")},
+        label = { Text( text = "Email or Username")},
         singleLine = true,
         colors = TextFieldDefaults.textFieldColors(
             backgroundColor = Color.Transparent,
