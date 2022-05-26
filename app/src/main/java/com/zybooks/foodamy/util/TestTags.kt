7c4d4416da8f3ad76e4e9e60b4e63887b2d1c18f
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-26 17:47:09 -0400

    Updated TestTags.kt with views from register screen

--- app/src/main/java/com/zybooks/foodamy/util/TestTags.kt
+++ app/src/main/java/com/zybooks/foodamy/util/TestTags.kt
@@ -1,6 +1,7 @@
 package com.zybooks.foodamy.util
 
 object TestTags {
+    //Login Screen
     const val Login_Facebook_Button = "login_facebook_button"
     const val Login_Google_Button = "login_google_button"
     const val Login_Email_Textfield = "login_email_textfield"
@@ -8,4 +9,12 @@ object TestTags {
     const val Sign_Up_Text_Click = "sign_up_text_click"
     const val Forgot_Password_Text_Click = "forgot_password_text_click"
     const val Login_Button = "login_button"
+
+    //Register Screen
+    const val Register_Facebook_Button = "register_facebook_button"
+    const val Register_Google_Button = "register_google_button"
+    const val Register_Username_Textfield = "register_username_textfield"
+    const val Register_Email_Textfield = "register_email_textfield"
+    const val Register_Password_Textfield = "register_password_textfield"
+    const val Register_Button = "register_button"
 }
