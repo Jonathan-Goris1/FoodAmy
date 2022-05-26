ac6ced15da8d3b9e10da53a1649eedaae76aaa54
Jonathan Goris <jonathangoris123@gmail.com>
2022-05-26 17:46:12 -0400

    Created Register state to hold values email, password and username

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/register_info/RegisterInfoState.kt
@@ -0,0 +1,9 @@
+package com.zybooks.foodamy.presentation.register_info
+
+data class RegisterInfoState(
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val isLoading: Boolean = false,
+    val error:String? = null
+)
