c795b43aea3cd2fbca41a997d7a59de7de949b30
Jonathan Goris <jonathangoris123@gmail.com>
2022-06-07 12:47:39 -0400

    Created a better composable for the textfield to handle the error better

--- app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoScreen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/login_info/LoginInfoScreen.kt
@@ -1,21 +1,21 @@
 package com.zybooks.foodamy.presentation.login_info
 
 import android.util.Log
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -28,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.zybooks.foodamy.ui.components.OutlineTextFieldWithError
 import com.zybooks.foodamy.ui.theme.DarkBlue
 import com.zybooks.foodamy.ui.theme.DarkGreen
 import com.zybooks.foodamy.ui.theme.DarkRed
@@ -41,24 +42,26 @@ fun LoginInfoScreen(
     ) {
     var passwordVisible by rememberSaveable { mutableStateOf(false) }
     val scaffoldState = rememberScaffoldState()
-
-    Scaffold(modifier = Modifier
-        .padding(0.dp, 0.dp, 0.dp, 16.dp),
-
+//TODO Change scaffold composable function to a lamda composable for better code reading
+//TODO also implement this function in a separate package called components
+    Scaffold(
+        modifier = Modifier
+            .padding(0.dp, 0.dp, 0.dp, 16.dp),
         topBar = {
             TopAppBar(
                 title = {
                     Box(
                         modifier = Modifier.fillMaxSize(),
                         contentAlignment = Alignment.Center
-                    ){
-                    Text("Login", textAlign = TextAlign.Center, color = Color.White) }
-                        },
+                    ) {
+                        Text("Login", textAlign = TextAlign.Center, color = Color.White)
+                    }
+                },
                 backgroundColor = DarkRed
             )
         },
         scaffoldState = scaffoldState
-    ){
+    ) {
 
         Column(
             modifier = Modifier
@@ -103,7 +106,7 @@ fun LoginInfoScreen(
             DividerText("Or")
 
 
-            OutlineTextFieldWithErrorView(
+            OutlineTextFieldWithError(
                 textStyle = TextStyle(textAlign = TextAlign.Left),
                 modifier = Modifier
                     .fillMaxWidth()
@@ -114,17 +117,16 @@ fun LoginInfoScreen(
                 label = { Text(text = "Email or Username") },
                 singleLine = true,
                 placeholder = { Text("Email or Username") },
-                isError = viewModel.validateEmail(),
-
+                isError = viewModel.state.isEmailError,
                 colors = TextFieldDefaults.textFieldColors(
                     backgroundColor = Color.Transparent,
                 ),
-                errorMsg = "Incorrect Email"
+                errorMessage = "Incorrect Email"
             )
 
 
 
-            OutlineTextFieldWithErrorView(
+            OutlineTextFieldWithError(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(0.dp, 0.dp, 0.dp, 16.dp)
@@ -134,7 +136,7 @@ fun LoginInfoScreen(
                 label = { Text("Password") },
                 singleLine = true,
                 placeholder = { Text("Password") },
-                isError = viewModel.validatePassword(),
+                isError = viewModel.state.isPasswordError,
                 colors = TextFieldDefaults.textFieldColors(
                     backgroundColor = Color.Transparent,
 
@@ -153,7 +155,7 @@ fun LoginInfoScreen(
                         Icon(imageVector = image, description)
                     }
                 },
-                errorMsg = "Incorrect Password"
+                errorMessage = "Incorrect Password"
             )
 
 
@@ -196,7 +198,7 @@ fun LoginInfoScreen(
                     .testTag(TestTags.Login_Button),
                 onClick = {
                     viewModel.validateEmail()
-
+                    viewModel.validatePassword()
                 },
                 enabled = true,
                 shape = MaterialTheme.shapes.medium,
@@ -208,8 +210,6 @@ fun LoginInfoScreen(
     }
 
 }
-
-
 
 
 @Composable
@@ -243,63 +243,6 @@ fun DividerText(prompt: String = "This is a Test") {
             thickness = 1.dp
         )
     }
-}
-
-@Composable
-fun OutlineTextFieldWithErrorView(
-    value: String,
-    onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    readOnly: Boolean = false,
-    textStyle: TextStyle = LocalTextStyle.current,
-    label: @Composable (() -> Unit)? = null,
-    placeholder: @Composable (() -> Unit)? = null,
-    leadingIcon: @Composable (() -> Unit)? = null,
-    trailingIcon: @Composable (() -> Unit)? = null,
-    isError: Boolean = false,
-    visualTransformation: VisualTransformation = VisualTransformation.None,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
-    singleLine: Boolean = false,
-    maxLines: Int = Int.MAX_VALUE,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    shape: Shape = MaterialTheme.shapes.small,
-    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(),
-    errorMsg : String = ""
-){
-
-
-        OutlinedTextField(
-            enabled = enabled,
-            readOnly = readOnly,
-            value = value,
-            onValueChange = onValueChange,
-            modifier = modifier,
-            singleLine = singleLine,
-            textStyle = textStyle,
-            label = label,
-            placeholder = placeholder,
-            leadingIcon = leadingIcon,
-            trailingIcon = trailingIcon,
-            isError = isError,
-            visualTransformation = visualTransformation,
-            keyboardOptions = keyboardOptions,
-            keyboardActions = keyboardActions,
-            maxLines = maxLines,
-            interactionSource = interactionSource,
-            shape = shape,
-            colors = colors
-        )
-
-        if (isError && value.isNotEmpty()){
-            Text(
-                text = errorMsg,
-                color = MaterialTheme.colors.error,
-                style = MaterialTheme.typography.caption,
-                modifier = Modifier.padding(start = 16.dp)
-            )
-        }
 }
 
 @Preview
@@ -346,7 +289,7 @@ fun LoginInfoScreenPreview() {
 
         DividerText("Or")
 
-        OutlineTextFieldWithErrorView(
+        OutlineTextFieldWithError(
             textStyle = TextStyle(textAlign = TextAlign.Left),
             modifier = Modifier
                 .fillMaxWidth()
@@ -357,12 +300,15 @@ fun LoginInfoScreenPreview() {
             label = { Text(text = "Email or Username") },
             singleLine = true,
             placeholder = { Text("Email or Username") },
+            isError = false,
+            errorMessage = "",
             colors = TextFieldDefaults.textFieldColors(
                 backgroundColor = Color.Transparent,
-            ),
+
+                ),
         )
 
-        OutlineTextFieldWithErrorView(
+        OutlineTextFieldWithError(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(0.dp, 0.dp, 0.dp, 16.dp)
@@ -378,6 +324,8 @@ fun LoginInfoScreenPreview() {
                 ),
             visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            isError = false,
+            errorMessage = "",
             trailingIcon = {
                 val image = if (passwordVisible)
                     Icons.Filled.Visibility
