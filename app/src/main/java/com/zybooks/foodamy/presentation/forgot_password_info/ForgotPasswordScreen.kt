878a4995cb7b67bc7292617412ba73e7c0b2eede
Jonathan Goris <jonathangoris123@gmail.com>
2022-06-09 17:35:09 -0400

    Removed textfield composable, optimized imports

--- app/src/main/java/com/zybooks/foodamy/presentation/forgot_password_info/ForgotPasswordScreen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/forgot_password_info/ForgotPasswordScreen.kt
@@ -1,23 +1,18 @@
 package com.zybooks.foodamy.presentation.forgot_password_info
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.zybooks.foodamy.ui.components.OutlineTextFieldWithError
 import com.zybooks.foodamy.ui.theme.DarkRed
 import com.zybooks.foodamy.util.TestTags
 
@@ -28,8 +23,9 @@ fun ForgotPasswordScreen(
     ) {
     val scaffoldState = rememberScaffoldState()
 
-    Scaffold(modifier = Modifier
-        .padding(0.dp, 0.dp, 0.dp, 16.dp),
+    Scaffold(
+        modifier = Modifier
+            .padding(0.dp, 0.dp, 0.dp, 16.dp),
 
         topBar = {
             TopAppBar(
@@ -37,8 +33,9 @@ fun ForgotPasswordScreen(
                     Box(
                         modifier = Modifier.fillMaxSize(),
                         contentAlignment = Alignment.Center
-                    ){
-                        Text("Forgot Password", textAlign = TextAlign.Center, color = Color.White) }
+                    ) {
+                        Text("Forgot Password", textAlign = TextAlign.Center, color = Color.White)
+                    }
                 },
                 backgroundColor = DarkRed
             )
@@ -53,7 +50,7 @@ fun ForgotPasswordScreen(
 
         ) {
 
-            OutlineTextFieldWithErrorView(
+            OutlineTextFieldWithError(
                 textStyle = TextStyle(textAlign = TextAlign.Left),
                 modifier = Modifier
                     .fillMaxWidth()
@@ -68,7 +65,7 @@ fun ForgotPasswordScreen(
                 colors = TextFieldDefaults.textFieldColors(
                     backgroundColor = Color.Transparent,
                 ),
-                errorMsg = "Incorrect Email"
+                errorMessage = "Incorrect Email"
             )
 
 
@@ -93,64 +90,6 @@ fun ForgotPasswordScreen(
 }
 
 
-
-@Composable
-fun OutlineTextFieldWithErrorView(
-    value: String,
-    onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    readOnly: Boolean = false,
-    textStyle: TextStyle = LocalTextStyle.current,
-    label: @Composable (() -> Unit)? = null,
-    placeholder: @Composable (() -> Unit)? = null,
-    leadingIcon: @Composable (() -> Unit)? = null,
-    trailingIcon: @Composable (() -> Unit)? = null,
-    isError: Boolean = false,
-    visualTransformation: VisualTransformation = VisualTransformation.None,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
-    singleLine: Boolean = false,
-    maxLines: Int = Int.MAX_VALUE,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    shape: Shape = MaterialTheme.shapes.small,
-    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(),
-    errorMsg : String = ""
-){
-
-
-    OutlinedTextField(
-        enabled = enabled,
-        readOnly = readOnly,
-        value = value,
-        onValueChange = onValueChange,
-        modifier = modifier,
-        singleLine = singleLine,
-        textStyle = textStyle,
-        label = label,
-        placeholder = placeholder,
-        leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon,
-        isError = isError,
-        visualTransformation = visualTransformation,
-        keyboardOptions = keyboardOptions,
-        keyboardActions = keyboardActions,
-        maxLines = maxLines,
-        interactionSource = interactionSource,
-        shape = shape,
-        colors = colors
-    )
-
-    if (isError){
-        Text(
-            text = errorMsg,
-            color = MaterialTheme.colors.error,
-            style = MaterialTheme.typography.caption,
-            modifier = Modifier.padding(start = 16.dp)
-        )
-    }
-}
-
 @Preview
 @Composable
 fun ForgotPasswordScreenPreview() {
@@ -162,7 +101,7 @@ fun ForgotPasswordScreenPreview() {
 
     ) {
 
-        OutlineTextFieldWithErrorView(
+        OutlineTextFieldWithError(
             textStyle = TextStyle(textAlign = TextAlign.Left),
             modifier = Modifier
                 .fillMaxWidth()
@@ -176,6 +115,9 @@ fun ForgotPasswordScreenPreview() {
             colors = TextFieldDefaults.textFieldColors(
                 backgroundColor = Color.Transparent,
             ),
+            errorMessage = "",
+            isError = false
+
         )
 
 
