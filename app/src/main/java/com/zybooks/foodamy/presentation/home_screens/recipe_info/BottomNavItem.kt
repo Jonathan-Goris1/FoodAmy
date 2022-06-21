2582bdecf1ed780b26372ae4411357f2582d45b0
Jonathan Goris <jonathangoris123@gmail.com>
2022-06-21 13:15:34 -0400

    Created a sealed class for the items of the bottom navigation menu

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/BottomNavItem.kt
@@ -0,0 +1,12 @@
+package com.zybooks.foodamy.presentation.home_screens.recipe_info
+
+import com.zybooks.foodamy.R
+
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Recipe : BottomNavItem("Recipe", R.drawable.ic_home,"recipe")
+    object Liked: BottomNavItem("Liked",R.drawable.ic_outline_navigation_24,"liked")
+    object Users: BottomNavItem("Users",R.drawable.ic_baseline_stars_24,"users")
+    object Menu: BottomNavItem("Menu",R.drawable.ic_baseline_menu_24,"menu")
+
+}
