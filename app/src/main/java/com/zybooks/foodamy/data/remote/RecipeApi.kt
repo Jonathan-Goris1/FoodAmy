4e7c5afa41712bbeb2dcbea3584c1bec47ade2bf
Jonathan Goris <jonathangoris123@gmail.com>
2022-06-23 15:21:58 -0400

    Added the correct EditorsChoice endpoint. Also removed @FormUrlEncoded as it was not needed.

--- app/src/main/java/com/zybooks/foodamy/data/remote/RecipeApi.kt
+++ app/src/main/java/com/zybooks/foodamy/data/remote/RecipeApi.kt
@@ -1,15 +1,17 @@
 package com.zybooks.foodamy.data.remote
 
 import com.zybooks.foodamy.data.remote.response.recipe.RecipeResponse
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface RecipeApi {
 
-    @FormUrlEncoded
+
     @GET("api/recipe/{id}")
     suspend fun getRecipe(
         @Path("recipeID") recipeID: Int,
     ): RecipeResponse
+
+    @GET("/api/editor-choices")
+    suspend fun getEditorsChoice(): RecipeResponse
 }
