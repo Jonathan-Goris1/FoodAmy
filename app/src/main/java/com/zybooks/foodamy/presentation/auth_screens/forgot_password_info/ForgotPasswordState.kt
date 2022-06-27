3d613533b93df0d429c02c84ca0c34d227a33067
Jonathan Goris <jonathangoris123@gmail.com>
2022-06-27 14:55:47 -0400

    Added snackBAr message as a variable state

--- app/src/main/java/com/zybooks/foodamy/presentation/auth_screens/forgot_password_info/ForgotPasswordState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/auth_screens/forgot_password_info/ForgotPasswordState.kt
@@ -2,6 +2,7 @@ package com.zybooks.foodamy.presentation.auth_screens.forgot_password_info
 
 data class ForgotPasswordState (
     val email: String = "",
+    val snackBarMessage: String = "",
     val isLoading: Boolean = false,
     val error: String? = null
         )
