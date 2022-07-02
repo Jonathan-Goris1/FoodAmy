ce6bb51f8b5a4189557198b08e1a248dab60998a
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-02 18:54:10 -0400

    Created a new NavHost for the Bottom navigation and deleted the extension function HomeNavGraph.kt

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/HomeHostNavGraph.kt
@@ -0,0 +1,37 @@
+package com.zybooks.foodamy.presentation.navigation.nav_graph
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.zybooks.foodamy.presentation.home_screens.liked_info.LikedScreen
+import com.zybooks.foodamy.presentation.home_screens.menu_info.MenuScreen
+import com.zybooks.foodamy.presentation.home_screens.recipe_info.RecipeHomeScreen
+import com.zybooks.foodamy.presentation.home_screens.users_info.UsersScreen
+import com.zybooks.foodamy.presentation.navigation.BottomNavItem
+
+@Composable
+
+fun HomeHostNavGraph(navController: NavController){
+
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = BottomNavItem.Recipe.screen_route,
+
+    ) {
+
+        composable(BottomNavItem.Recipe.screen_route) {
+            RecipeHomeScreen()
+        }
+        composable(BottomNavItem.Liked.screen_route) {
+            LikedScreen()
+        }
+        composable(BottomNavItem.Users.screen_route) {
+            UsersScreen()
+        }
+        composable(BottomNavItem.Menu.screen_route) {
+            MenuScreen()
+        }
+    }
+}
