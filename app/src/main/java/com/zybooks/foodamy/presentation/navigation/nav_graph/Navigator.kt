6b4c502b9235c014169e1b5e65e8ff6c66df43c4
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-02 18:57:11 -0400

    completed the TODO removed and deleted homeNavGraph Made AppScaffold its own composable because it will take in a different NavHost

--- app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/Navigator.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/Navigator.kt
@@ -2,21 +2,32 @@ package com.zybooks.foodamy.presentation.navigation.nav_graph
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.zybooks.foodamy.presentation.home_screens.recipe_info.AppScaffold
 import com.zybooks.foodamy.presentation.navigation.AUTHENTICATION_ROUTE
-import com.zybooks.foodamy.presentation.navigation.ROOT_ROUTE
+import com.zybooks.foodamy.presentation.navigation.Screen
 
 @Composable
 fun Navigator(){
     val navController = rememberNavController()
+    val navBarController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = AUTHENTICATION_ROUTE,
-        route = ROOT_ROUTE
+        startDestination = AUTHENTICATION_ROUTE
+
     ) {
 
         authNavGraph(navController = navController)
-        //TODO("Fix navigation issue, maybe find a different way of getting the navigation for bottomNavBar ")
-        homeNavGraph(navController = navController)
+
+        //homeNavGraph(navController = navController)
+        composable(
+            route = Screen.AppScaffold.route,
+            content = { AppScaffold(navController = navBarController) }
+        )
+
+
+
     }
+
 }
