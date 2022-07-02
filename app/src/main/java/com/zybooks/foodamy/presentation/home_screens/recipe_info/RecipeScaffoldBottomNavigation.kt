09db59684774323b30c0c751ef71b4f1e5acc4c3
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-02 18:59:26 -0400

    Rather than creating the scaffold for the bottom navigation within RecipeScreen.kt, I decided to make it its own file which helped fix the navigation issues

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScaffoldBottomNavigation.kt
@@ -0,0 +1,32 @@
+package com.zybooks.foodamy.presentation.home_screens.recipe_info
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.zybooks.foodamy.presentation.navigation.nav_graph.HomeHostNavGraph
+import com.zybooks.foodamy.ui.components.RecipeBottomNavigation
+
+
+@Composable
+fun AppScaffold(
+    navController: NavController,
+
+) {
+    val scaffoldState = rememberScaffoldState()
+
+
+    Scaffold(
+        bottomBar = {
+            RecipeBottomNavigation(navController = navController)
+        },
+        scaffoldState = scaffoldState,
+
+        ) {
+
+        HomeHostNavGraph(navController = navController)
+
+
+    }
+
+}
