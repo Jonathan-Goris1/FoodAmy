3c0c3456fbd7bf7f3023f890f3f8dc63cf0317d2
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-01 20:36:18 -0400

    Added constants for nested navigation

--- app/src/main/java/com/zybooks/foodamy/presentation/navigation/Screen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/navigation/Screen.kt
@@ -1,8 +1,17 @@
 package com.zybooks.foodamy.presentation.navigation
 
+
+const val AUTHENTICATION_ROUTE = "authentication"
+const val HOME_ROUTE = "home"
+const val ROOT_ROUTE = "root"
+
 sealed class Screen(val route: String){
+
     object Login : Screen("login")
     object ForgotPassword : Screen("forgot_password")
     object Register: Screen("register")
     object Home: Screen("home")
+
 }
+
+
