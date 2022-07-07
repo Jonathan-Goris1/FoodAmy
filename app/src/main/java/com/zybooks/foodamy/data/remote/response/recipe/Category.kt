105b007c551683896815dd3b2925e4a18598e89d
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-07 15:57:30 -0400

    Made image nullable

--- app/src/main/java/com/zybooks/foodamy/data/remote/response/recipe/Category.kt
+++ app/src/main/java/com/zybooks/foodamy/data/remote/response/recipe/Category.kt
@@ -2,7 +2,7 @@ package com.zybooks.foodamy.data.remote.response.recipe
 
 data class Category(
     val id: Int,
-    val image: Image,
+    val image: Image?,
     val language: String,
     val main_category_id: Any,
     val name: String
