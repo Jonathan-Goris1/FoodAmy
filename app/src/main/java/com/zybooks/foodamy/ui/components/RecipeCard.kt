e412fc7015754257c72bb81373abfeb9ed4a5e0f
Jonathan Goris <jonathangoris123@gmail.com>
2022-07-07 16:00:30 -0400

    MAde some corrections to the image sizes. Added clip modifier to alter image shape. Removed null if statements and made the data nullable instead.

--- app/src/main/java/com/zybooks/foodamy/ui/components/RecipeCard.kt
+++ app/src/main/java/com/zybooks/foodamy/ui/components/RecipeCard.kt
@@ -3,12 +3,15 @@ package com.zybooks.foodamy.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,7 +25,7 @@ import com.zybooks.foodamy.data.remote.response.recipe.Data
 @Composable
 fun RecipeCard(
     modifier: Modifier = Modifier,
-    Recipe: Data?,
+    Recipe: Data,
     onClick: () -> Unit
 
 ) {
@@ -32,28 +35,34 @@ fun RecipeCard(
             .clickable(onClick = onClick)
     ) {
 
-        Column() {
+        Column {
 
-            Row(modifier = modifier.padding(8.dp)) {
-                if (Recipe != null) {
-                    Image(
-                        modifier = modifier
-                            .padding(8.dp),
-                        painter = rememberAsyncImagePainter(Recipe.user.image.url),
-                        contentDescription = null
+            Row(
+                modifier = modifier.padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Image(
+                    modifier = modifier
+                        .width(80.dp)
+                        .height(80.dp)
+                        .padding(8.dp)
+                        .clip(shape = CircleShape),
+                    painter = rememberAsyncImagePainter(Recipe.user.image?.url),
+                    contentDescription = null,
+
                     )
-                }
+
 
                 Column {
-                    if (Recipe != null) {
-                        Text(text = Recipe.user.name)
-                    }
+                    Text(text = Recipe.user.name ?: "")
 
-                    Spacer(modifier = modifier.size(8.dp))
 
-                    if (Recipe != null) {
-                        Text(text = "${Recipe.user.recipe_count} Recipes ${Recipe.user.following_count} Followers")
-                    }
+                    Spacer(modifier = modifier.size(4.dp))
+
+
+                    Text(text = "${Recipe.user.recipe_count} Recipes ${Recipe.user.following_count} Followers")
+
 
                 }
             }
@@ -68,38 +77,39 @@ fun RecipeCard(
                     )
                 }
 
-                if (Recipe != null) {
-                    Text(
-                        modifier = modifier.padding(8.dp),
-                        text = Recipe.title,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
 
-                if (Recipe != null) {
-                    Text(
-                        modifier = modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
-                        text = Recipe.category.name,
-                        fontSize = 12.sp
-                    )
-                }
+                Text(
+                    modifier = modifier.padding(8.dp),
+                    text = Recipe.title,
+                    fontWeight = FontWeight.Bold
+                )
 
-                if (Recipe != null) {
-                    Image(
-                        modifier = modifier
-                            .width(400.dp)
-                            .height(400.dp)
-                            .padding(8.dp),
-                        painter = rememberAsyncImagePainter(Recipe.category.image.url),
-                        contentDescription = null
-                    )
-                }
-                if (Recipe != null) {
-                    Text(
-                        modifier = modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
-                        text = "${Recipe.comment_count} Comments ${Recipe.like_count} Taste"
-                    )
-                }
+
+
+                Text(
+                    modifier = modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
+                    text = Recipe.category.name,
+                    fontSize = 12.sp
+                )
+
+
+
+                Image(
+                    modifier = modifier
+                        .width(400.dp)
+                        .height(400.dp)
+                        .padding(8.dp)
+                        .clip(shape = RoundedCornerShape(4.dp)),
+                    painter = rememberAsyncImagePainter(Recipe.category.image?.url),
+                    contentDescription = null
+                )
+
+
+                Text(
+                    modifier = modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
+                    text = "${Recipe.comment_count} Comments ${Recipe.like_count} Taste"
+                )
+
             }
 
         }
@@ -122,15 +132,17 @@ fun RecipeCardPreview() {
             ) {
                 Image(
                     modifier = Modifier
+                        .width(80.dp)
+                        .height(80.dp)
                         .padding(8.dp)
-                        .size(50.dp, 50.dp),
+                        .clip(shape = CircleShape),
                     painter = painterResource(id = R.drawable.ic_baseline_face_24),
                     contentDescription = null
                 )
 
                 Column {
                     Text(text = "Joseph")
-                    Spacer(modifier = Modifier.size(8.dp))
+                    Spacer(modifier = Modifier.size(4.dp))
                     Text(text = "0 Recipes 0 Followers")
 
                 }
@@ -161,7 +173,8 @@ fun RecipeCardPreview() {
                 modifier = Modifier
                     .width(400.dp)
                     .height(400.dp)
-                    .padding(8.dp),
+                    .padding(8.dp)
+                    .clip(shape = RoundedCornerShape(4.dp)),
                 painter = painterResource(id = R.drawable.ic_baseline_face_24),
                 contentDescription = null
             )
@@ -170,7 +183,6 @@ fun RecipeCardPreview() {
                 modifier = Modifier.padding(8.dp, 0.dp, 0.dp, 0.dp),
                 text = "0 Comments 0 Taste"
             )
-
 
         }
 
