4e6163c595e105a29e812a653d6fb225decc3117
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-17 14:30:32 -0400

    created a new suspend function to retrieve all the data in the database

--- app/src/main/java/com/zybooks/foodamy/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/zybooks/foodamy/data/repository/RecipeRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.zybooks.foodamy.data.repository
 
+import com.zybooks.foodamy.data.local.dao.RecipeDao
+import com.zybooks.foodamy.data.local.local_dto.RecipeDb
 import com.zybooks.foodamy.data.remote.network_api.RecipeApi
-import com.zybooks.foodamy.data.remote.response.recipe.Data
 import com.zybooks.foodamy.data.remote.response.recipe.RecipeResponse
+import com.zybooks.foodamy.data.remote.response.recipe.DataResponse
 import com.zybooks.foodamy.domain.repository.RecipeRepository
 import com.zybooks.foodamy.util.Resource
 import retrofit2.HttpException
@@ -12,11 +14,18 @@ import javax.inject.Singleton
 
 @Singleton
 class RecipeRepositoryImpl @Inject constructor(
-    private val api: RecipeApi
+    private val api: RecipeApi,
+    private val dao: RecipeDao
+
 ): RecipeRepository {
 
+    private suspend fun getRecipeFromDatabase(): Resource<List<RecipeDb>>{
+        return dao.getRecipeListing()
 
-    override suspend fun getRecipeInfo(recipeID: Int): Resource<RecipeResponse> {
+    }
+
+
+    override suspend fun getRecipeInfo(recipeID: Int): Resource<DataResponse> {
         return try {
             val result = api.getRecipe(recipeID)
             Resource.Success(result)
@@ -35,9 +44,9 @@ class RecipeRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun getEditorsChoiceInfo(): Resource<List<Data>> {
+    override suspend fun getEditorsChoiceInfo(): Resource<List<RecipeResponse>> {
         return try {
-            val result = api.getEditorsChoice().data
+            val result = api.getEditorsChoice().recipe
             Resource.Success(result)
         } catch (e: IOException){
             e.printStackTrace()
