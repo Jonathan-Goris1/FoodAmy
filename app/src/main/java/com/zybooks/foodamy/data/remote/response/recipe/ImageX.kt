d2105ae7828d1303880afd889831ddc21287a165
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-17 14:25:48 -0400

    ImageX.kt and ImageXX.kt have been deleted since they are the same as ImageResponse

--- app/src/main/java/com/zybooks/foodamy/data/remote/response/recipe/ImageX.kt
+++
@@ -1,9 +0,0 @@
-package com.zybooks.foodamy.data.remote.response.recipe
-
-data class ImageX(
-    val height: Int,
-    val key: String,
-    val order: Int,
-    val url: String,
-    val width: Int
-)
--- app/src/main/java/com/zybooks/foodamy/data/remote/response/recipe/ImageXX.kt
+++
@@ -1,9 +0,0 @@
-package com.zybooks.foodamy.data.remote.response.recipe
-
-data class ImageXX(
-    val height: Int,
-    val key: String,
-    val order: Int,
-    val url: String,
-    val width: Int
-)
