22b4ac16a01c109fe3d10e8164eaad33e000751f
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-17 14:27:20 -0400

    Updated Parameters and included more UI

--- app/src/main/java/com/zybooks/foodamy/ui/components/LikeScreenCard.kt
+++ app/src/main/java/com/zybooks/foodamy/ui/components/LikeScreenCard.kt
@@ -3,7 +3,8 @@ package com.zybooks.foodamy.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
@@ -14,16 +15,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.zybooks.foodamy.R
-import com.zybooks.foodamy.data.remote.response.recipe.Data
+import com.zybooks.foodamy.data.remote.response.recipe.RecipeResponse
 
 @Composable
 fun LikedScreenCard(
     modifier: Modifier = Modifier,
-    Recipe: Data,
+    Recipe: RecipeResponse,
+    likesData: List<RecipeResponse>,
     onClick: () -> Unit
 ) {
     Card(
@@ -38,29 +41,42 @@ fun LikedScreenCard(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
-                    modifier = modifier
-                        .width(80.dp)
-                        .height(80.dp)
+                    modifier = Modifier
+                        .width(50.dp)
+                        .height(50.dp)
                         .padding(8.dp)
-                        .clip(shape = CircleShape),
+                        .clip(shape = RectangleShape),
                     painter = rememberAsyncImagePainter(Recipe.category.image?.url),
                     contentDescription = null,
                 )
 
                 Text(text = Recipe.category.name)
 
-                Text(text = "See All")
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    textAlign = TextAlign.End,
+                    text = "See All")
 
             }
-
-            Row(modifier = Modifier.fillMaxWidth()) {
                 Divider(
                     modifier = Modifier
-                        .fillMaxWidth(),
+                        .fillMaxWidth()
+                        .padding(0.dp, 16.dp),
                     color = Color.LightGray,
                     thickness = 1.dp
                 )
+
+            LazyRow {
+                items(likesData) { favorite ->
+                    LikesCard(modifier = Modifier, Recipe = favorite)
+
+                }
+
             }
+            
+            
+
         }
 
     }
@@ -70,42 +86,57 @@ fun LikedScreenCard(
 @Preview
 @Composable
 fun LikedScreenCardPreview() {
+
     Card(
         modifier = Modifier
             .padding(0.dp, 8.dp)
+
     ) {
-        Column {
+        Column(modifier = Modifier
+            .fillMaxWidth(),
+        ) {
 
             Row(
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier
+                    .padding(8.dp, 0.dp)
+                    .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Image(
                     modifier = Modifier
-                        .width(80.dp)
-                        .height(80.dp)
+                        .width(50.dp)
+                        .height(50.dp)
                         .padding(8.dp)
                         .clip(shape = RectangleShape),
-                    painter = painterResource(id = R.drawable.ic_baseline_face_24),
+                    painter = painterResource(R.drawable.ic_baseline_face_24),
                     contentDescription = null,
                 )
 
-                Text(text = "ET")
+                Text(
+                    text = "Meat"
+                )
 
-                Text(text = "See All")
+
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    textAlign = TextAlign.End,
+                    text = "See All")
 
             }
 
-            Row(modifier = Modifier.fillMaxWidth()) {
                 Divider(
                     modifier = Modifier
-                        .fillMaxWidth(),
+                        .fillMaxWidth()
+                        .padding(0.dp, 16.dp),
                     color = Color.LightGray,
                     thickness = 1.dp
                 )
-            }
+
         }
 
     }
 
 }
+
+
