683dd140b9997ed45347cc1c6aa7e618867ff8aa
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-19 13:15:40 -0400

    Changed the recipes parameter type from RecipeResponse to Recipe

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScreenState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScreenState.kt
@@ -1,9 +1,9 @@
 package com.zybooks.foodamy.presentation.home_screens.recipe_info
 
-import com.zybooks.foodamy.data.remote.response.recipe.RecipeResponse
+import com.zybooks.foodamy.domain.repository.model.Recipe
 
 data class RecipeScreenState(
     val isLoading: Boolean = false,
     val error: String? = null,
-    val recipes: List<RecipeResponse> = emptyList()
+    val recipes: List<Recipe> = emptyList()
 )
