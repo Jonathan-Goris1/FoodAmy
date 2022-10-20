724dd0d1f17bbdea2ed9ba175d3dfaf4f7f259b6
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:08:30 -0400

    removed unused properties

--- app/src/main/java/com/zybooks/foodamy/domain/repository/model/Recipe.kt
+++ app/src/main/java/com/zybooks/foodamy/domain/repository/model/Recipe.kt
@@ -1,20 +1,20 @@
 package com.zybooks.foodamy.domain.repository.model
 
 data class Recipe(
-    val category: Category,
-    val comment_count: Int,
-    val definition: String,
-    val directions: String,
     val id: Int,
-    val images: List<Image>,
-    val ingredients: String,
-    val is_editor_choice: Boolean,
-    val language: String,
-    val like_count: Int,
-    val number_of_favorite_count: Int,
-    val number_of_person: NumberOfPerson,
-    val time_of_recipe: TimeOfRecipe,
     val title: String,
+    val definition: String,
+    val ingredients: String,
+    val directions: String,
+    val difference: String,
+    val isEditorChoice: Boolean,
+    val isLiked: Boolean,
+    val likeCount: Int,
+    val commentCount: Int,
     val user: User,
+    val timeOfRecipe: TimeOfRecipe,
+    val numberOfPerson: NumberOfPerson,
+    val category: Category,
+    val images: List<Image>
 
 )
