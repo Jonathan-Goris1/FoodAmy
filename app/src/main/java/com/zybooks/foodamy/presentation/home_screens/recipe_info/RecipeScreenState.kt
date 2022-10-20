8a219d163adb5b12dc15fe4086c1d9f803a5692b
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:18:03 -0400

    State property recipes type has changed to PagingData<Recipe> and default value to PagingData.empty()

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScreenState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScreenState.kt
@@ -1,9 +1,10 @@
 package com.zybooks.foodamy.presentation.home_screens.recipe_info
 
+import androidx.paging.PagingData
 import com.zybooks.foodamy.domain.repository.model.Recipe
 
 data class RecipeScreenState(
     val isLoading: Boolean = false,
     val error: String? = null,
-    val recipes: List<Recipe> = emptyList()
+    val recipes: PagingData<Recipe> = PagingData.empty()
 )
