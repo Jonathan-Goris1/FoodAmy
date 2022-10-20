83def03f8dfafdfdac064e1c6e9a92519bed1e74
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 15:55:49 -0400

    Added more entities and type converters and RemoteKeysDao.kt

--- app/src/main/java/com/zybooks/foodamy/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/zybooks/foodamy/data/local/database/AppDatabase.kt
@@ -2,16 +2,38 @@ package com.zybooks.foodamy.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.zybooks.foodamy.data.local.converters.CategoryConverter
+import com.zybooks.foodamy.data.local.converters.ImageListConverter
+import com.zybooks.foodamy.data.local.converters.RecipeListConverter
+import com.zybooks.foodamy.data.local.converters.UserConverter
 import com.zybooks.foodamy.data.local.dao.RecipeDao
-import com.zybooks.foodamy.data.local.local_dto.RecipeDb
+import com.zybooks.foodamy.data.local.dao.RemoteKeysDao
+import com.zybooks.foodamy.data.local.local_dto.*
 
 @Database(
-    entities = [RecipeDb::class],
+    entities = [
+        RecipeDb::class,
+        CategoryDb::class,
+        CommentDb::class,
+        RemoteKeyComment::class,
+        UserDb::class,
+        RemoteKeysEditor::class,
+               ],
     version = 1,
     exportSchema = false
 )
+
+@TypeConverters(
+    UserConverter::class,
+    CategoryConverter::class,
+    RecipeListConverter::class,
+    ImageListConverter::class,
+
+)
 abstract class AppDatabase: RoomDatabase() {
-    abstract val dao: RecipeDao
+    abstract val recipeDao: RecipeDao
+    abstract fun remoteKeysDao(): RemoteKeysDao
 }
 
 
