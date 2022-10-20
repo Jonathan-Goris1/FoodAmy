0d6709faa758ab14073440f466046744b952546e
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:24:26 -0400

    created a new module for room database

---
+++ app/src/main/java/com/zybooks/foodamy/di/RoomModule.kt
@@ -0,0 +1,32 @@
+package com.zybooks.foodamy.di
+
+import android.app.Application
+import androidx.room.Room
+import com.zybooks.foodamy.data.local.dao.RecipeDao
+import com.zybooks.foodamy.data.local.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Provides
+    @Singleton
+    fun provideRecipeDao(appDatabase: AppDatabase): RecipeDao {
+        return appDatabase.recipeDao
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecipeDatabase(app: Application): AppDatabase {
+        return Room.databaseBuilder(
+            app,
+            AppDatabase::class.java,
+            "recipeDB.db"
+        ).build()
+    }
+}
