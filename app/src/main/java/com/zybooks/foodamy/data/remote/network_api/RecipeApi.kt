4d44f00e93e0030246cafbdd1dafea3b4a7d5c2a
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:09:02 -0400

    changed geteditorchoice return type from data to dataresponse

--- app/src/main/java/com/zybooks/foodamy/data/remote/network_api/RecipeApi.kt
+++ app/src/main/java/com/zybooks/foodamy/data/remote/network_api/RecipeApi.kt
@@ -1,9 +1,9 @@
 package com.zybooks.foodamy.data.remote.network_api
 
 import com.zybooks.foodamy.data.remote.response.recipe.DataResponse
-import com.zybooks.foodamy.domain.repository.model.Data
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RecipeApi {
 
@@ -14,5 +14,7 @@ interface RecipeApi {
     ): DataResponse
 
     @GET("editor-choices")
-    suspend fun getEditorsChoice(): Data
+    suspend fun getEditorsChoice(
+        @Query("page") page: Int
+    ): DataResponse
 }
