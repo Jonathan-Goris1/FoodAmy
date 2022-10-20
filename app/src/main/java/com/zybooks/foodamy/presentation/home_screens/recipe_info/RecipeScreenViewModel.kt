85a83d18b70fe271d4a490b71ad0cb41f1da01d6
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:21:44 -0400

    Updated the viewmodel to request a database and not a network

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScreenViewModel.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_info/RecipeScreenViewModel.kt
@@ -1,52 +1,42 @@
 package com.zybooks.foodamy.presentation.home_screens.recipe_info
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.zybooks.foodamy.domain.repository.RecipeRepository
-import com.zybooks.foodamy.util.Resource
+import com.zybooks.foodamy.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RecipeScreenViewModel@Inject constructor(
+class RecipeScreenViewModel @Inject constructor(
     private val repository: RecipeRepository
-): ViewModel() {
+) : BaseViewModel() {
     var state by mutableStateOf(RecipeScreenState())
     private val tag: String = "RecipeScreenViewModel"
 
-init {
-    getEditorsChoice()
-}
+    init {
+        getEditorsChoice()
+    }
 
 
     private fun getEditorsChoice() {
-        viewModelScope.launch {
-            state = state.copy(isLoading = true)
-            when (val getEditorsChoiceInfoResult = repository.getEditorsChoiceInfo()) {
-                is Resource.Success -> {
+        sendRequest(
+            request = {
+                repository.getEditorChoicePaging()
+            },
+            success = {
+                it.cachedIn(viewModelScope).collect { pagingData ->
                     state = state.copy(
-                        recipes = getEditorsChoiceInfoResult.data?: emptyList(),
                         isLoading = false,
+                        recipes = pagingData,
                         error = null
                     )
                 }
-                is Resource.Error -> {
-                    state = state.copy(
-                        isLoading = false,
-                        error = getEditorsChoiceInfoResult.message,
-
-                    )
-                    Log.d(tag, state.error.toString())
-
-                }
-                else -> {}
             }
-            state = state.copy(isLoading = false)
-        }
+        )
+
     }
 }
