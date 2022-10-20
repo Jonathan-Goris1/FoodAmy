ae8e0000511de8d952156c7a3cab03c0aab569eb
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:09:36 -0400

    fixed getEditorChoicesPaging function

--- app/src/main/java/com/zybooks/foodamy/data/local/dao/RecipeDao.kt
+++ app/src/main/java/com/zybooks/foodamy/data/local/dao/RecipeDao.kt
@@ -1,23 +1,26 @@
 package com.zybooks.foodamy.data.local.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.zybooks.foodamy.data.local.local_dto.RecipeDb
-import com.zybooks.foodamy.util.Resource
 
 @Dao
 interface RecipeDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertData(
-        recipes: List<RecipeDb>
-    )
+    suspend fun insertRecipes(recipes: List<RecipeDb>)
+
+
+
+    @Query("select * from recipes where is_editor_choice = 1 order by id desc")
+    fun getEditorChoicesPaging(): PagingSource<Int, RecipeDb>
 
     @Query("DELETE FROM recipes")
     suspend fun clearRecipeListing()
 
-    @Query("SELECT * FROM recipes ")
-    suspend fun getRecipeListing(): Resource<List<RecipeDb>>
+    @Query("delete from recipes where is_editor_choice = 1")
+    suspend fun deleteEditorChoices()
 
 }
