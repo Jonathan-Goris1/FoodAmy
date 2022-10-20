9f8c6876d81a8f69a8cacc18b9d505048f9cdaf2
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:07:08 -0400

    removed key and order property because it wont be used and added a companion object for empty db

--- app/src/main/java/com/zybooks/foodamy/data/local/local_dto/ImageDb.kt
+++ app/src/main/java/com/zybooks/foodamy/data/local/local_dto/ImageDb.kt
@@ -2,8 +2,12 @@ package com.zybooks.foodamy.data.local.local_dto
 
 data class ImageDb(
     val height: Int,
-    val key: String,
-    val order: Int,
     val url: String,
     val width: Int
-)
+){
+    companion object {
+        val EMPTY = ImageDb(0, "", 0)
+    }
+}
+
+
