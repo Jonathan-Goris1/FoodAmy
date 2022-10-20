2d12fa28e5fa6ef8fcb5faedaf461f7158931743
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:22:53 -0400

    created a RemoteKey dao

---
+++ app/src/main/java/com/zybooks/foodamy/data/local/dao/RemoteKeysDao.kt
@@ -0,0 +1,36 @@
+package com.zybooks.foodamy.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.zybooks.foodamy.data.local.local_dto.RemoteKeyComment
+import com.zybooks.foodamy.data.local.local_dto.RemoteKeysEditor
+
+@Dao
+interface RemoteKeysDao {
+
+    // EditorChoices Recipes Remote Keys
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEditorRemoteKeys(remoteKeys: List<RemoteKeysEditor>)
+
+    @Query("select * from remote_keys_editor where id =:id")
+    suspend fun getEditorRemoteKeys(id: Int): RemoteKeysEditor
+
+    @Query("delete from remote_keys_editor")
+    suspend fun deleteEditorKeys()
+
+
+
+    // Recipe Comments Remote Key
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCommentsRemoteKeys(remoteKeys: List<RemoteKeyComment>)
+
+    @Query("select * from remote_key_comments where id =:id")
+    suspend fun getCommentsRemoteKey(id: Int): RemoteKeyComment
+
+    @Query("delete from remote_key_comments")
+    suspend fun deleteCommentsRemoteKeys()
+
+
+}
