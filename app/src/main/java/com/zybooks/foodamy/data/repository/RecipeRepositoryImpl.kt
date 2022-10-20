7795811ddc57dd302bc039b0eb9dc9ede196247d
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:12:35 -0400

    changed getEditorsChoicePaging return type to Flow<PagingData<Recipe>> and changed function to call database instead of network

--- app/src/main/java/com/zybooks/foodamy/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/zybooks/foodamy/data/repository/RecipeRepositoryImpl.kt
@@ -1,33 +1,41 @@
 package com.zybooks.foodamy.data.repository
 
+import androidx.paging.*
 import com.zybooks.foodamy.data.local.dao.RecipeDao
-import com.zybooks.foodamy.data.local.local_dto.RecipeDb
+import com.zybooks.foodamy.data.local.dao.RemoteKeysDao
+import com.zybooks.foodamy.data.mapper.toDomainModel
 import com.zybooks.foodamy.data.remote.network_api.RecipeApi
 import com.zybooks.foodamy.data.remote.response.recipe.DataResponse
+import com.zybooks.foodamy.data.repository.base.BaseRepository
+import com.zybooks.foodamy.data.utils.RecipeEditorRemoteMediator
 import com.zybooks.foodamy.domain.repository.RecipeRepository
 import com.zybooks.foodamy.domain.repository.model.Recipe
 import com.zybooks.foodamy.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
+
+@OptIn(ExperimentalPagingApi::class)
 class RecipeRepositoryImpl @Inject constructor(
-    private val api: RecipeApi,
-    private val dao: RecipeDao
+    private val recipeApi: RecipeApi,
+    private val recipeDao: RecipeDao,
+    private val remoteKeysDao: RemoteKeysDao,
 
-): RecipeRepository {
+    ): RecipeRepository, BaseRepository() {
 
-    private suspend fun getRecipeFromDatabase(): Resource<List<RecipeDb>>{
-        return dao.getRecipeListing()
-
-    }
-
+    private val pageConfig = PagingConfig(
+        pageSize = PAGE_SIZE,
+        maxSize = MAX_SIZE,
+        enablePlaceholders = false
+    )
 
     override suspend fun getRecipeInfo(recipeID: Int): Resource<DataResponse> {
+
         return try {
-            val result = api.getRecipe(recipeID)
+            val result = recipeApi.getRecipe(recipeID)
             Resource.Success(result)
         } catch (e: IOException){
             e.printStackTrace()
@@ -42,24 +50,33 @@ class RecipeRepositoryImpl @Inject constructor(
             )
 
         }
-    }
 
-    override suspend fun getEditorsChoiceInfo(): Resource<List<Recipe>> {
-        return try {
-            val result = api.getEditorsChoice().recipe
-            Resource.Success(result)
-        } catch (e: IOException){
-            e.printStackTrace()
-            Resource.Error(
-                message = e.message.toString()
-            )
-
-        } catch (e: HttpException){
-            e.printStackTrace()
-            Resource.Error(
-                message = e.message()
-            )
-
-        }
-    }
 }
+
+
+
+    override suspend fun getEditorChoicePaging(): Flow<PagingData<Recipe>> =
+        execute {
+            val pagingSourceFactory = { recipeDao.getEditorChoicesPaging() }
+            Pager(
+                config = pageConfig,
+                remoteMediator = RecipeEditorRemoteMediator(
+                    recipeApi = recipeApi,
+                    recipeDao = recipeDao,
+                    remoteKeysDao = remoteKeysDao
+                ),
+                pagingSourceFactory = pagingSourceFactory
+            ).flow.map { pagingData ->
+                pagingData.map {
+                    it.toDomainModel()
+                }
+            }
+        }
+
+    companion object {
+        private const val PAGE_SIZE = 24
+        private const val MAX_SIZE = 100
+    }
+
+}
+
