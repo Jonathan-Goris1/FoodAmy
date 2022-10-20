5a58910409268a147a670bb9854c4bcf89629284
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-20 16:07:25 -0400

    a converter for imageDb

---
+++ app/src/main/java/com/zybooks/foodamy/data/local/converters/ImageListConverter.kt
@@ -0,0 +1,19 @@
+package com.zybooks.foodamy.data.local.converters
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.zybooks.foodamy.data.local.local_dto.ImageDb
+
+@ProvidedTypeConverter
+class ImageListConverter {
+
+    @TypeConverter
+    fun imageListToJson(images: List<ImageDb>): String {
+        return toJson<List<ImageDb>>(images)
+    }
+
+    @TypeConverter
+    fun jsonToImageList(imageListSrc: String): List<ImageDb> {
+        return fromJson(imageListSrc)
+    }
+}
