0b7734317f49fb818c8d8aa2ee09462b975bf329
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-26 13:10:47 -0400

    Change class type to inherit from BaseViewModel changed function login to follow inheritance

--- app/src/main/java/com/zybooks/foodamy/presentation/auth_screens/login_info/LoginInfoViewModel.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/auth_screens/login_info/LoginInfoViewModel.kt
@@ -3,20 +3,17 @@ package com.zybooks.foodamy.presentation.auth_screens.login_info
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.zybooks.foodamy.domain.repository.AuthRepository
-import com.zybooks.foodamy.util.Resource
+import com.zybooks.foodamy.presentation.base.BaseViewModel
 import com.zybooks.foodamy.util.Validations.validateEmail
 import com.zybooks.foodamy.util.Validations.validatePassword
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginInfoViewModel @Inject constructor(
-    private val repository: AuthRepository
-) : ViewModel() {
+    private val authRepository: AuthRepository
+) : BaseViewModel() {
 
     var state by mutableStateOf(LoginInfoState())
 
@@ -30,30 +27,13 @@ class LoginInfoViewModel @Inject constructor(
 
     fun login() {
         if (validateEmail(state.email) && validatePassword(state.password)) {
-            viewModelScope.launch {
-                state = state.copy(isLoading = true)
-                when (val loginInfoResult = repository.postLoginInfo(state.email, state.password)) {
-                    is Resource.Success -> {
-                        state = state.copy(
-                            snackBarMessage = "Login Successful",
-                            isLoading = false,
-                            error = null
-                        )
-                    }
-                    is Resource.Error -> {
-                        state = state.copy(
-                            snackBarMessage = "Invalid email or password",
-                            isLoading = false,
-                            error = loginInfoResult.message,
-                        )
+            sendRequest(
+                loading = true,
+                request = { authRepository.login(state.email, state.password) },
+                success = {
 
-                    }
-                    else -> {}
-                }
-                state = state.copy(isLoading = false)
+                })
             }
-        } else{
-            state = state.copy(snackBarMessage = "Please enter a correct email or password")
-        }
+
     }
 }
