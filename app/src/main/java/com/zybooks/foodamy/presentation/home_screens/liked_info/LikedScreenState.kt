cd92e0ba3ffe0f386cc95aacd75854ec60d95d24
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-28 15:51:16 -0400

    Added state parameters

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreenState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreenState.kt
@@ -1,4 +1,12 @@
 package com.zybooks.foodamy.presentation.home_screens.liked_info
 
-class LikedScreenState {
-}
+import androidx.paging.PagingData
+import com.zybooks.foodamy.domain.model.Recipe
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class LikedScreenState (
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val recipes: Flow<PagingData<Recipe>> = emptyFlow()
+)
