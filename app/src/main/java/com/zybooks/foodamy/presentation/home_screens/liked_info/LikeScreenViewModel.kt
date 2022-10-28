206aa4747ba688d74a62ed481254b6a8468b16b2
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-28 15:51:38 -0400

    Created the function to retrieve the api call

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikeScreenViewModel.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikeScreenViewModel.kt
@@ -1,4 +1,43 @@
 package com.zybooks.foodamy.presentation.home_screens.liked_info
 
-class LikeScreenViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.zybooks.foodamy.domain.repository.RecipeRepository
+import com.zybooks.foodamy.presentation.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LikeScreenViewModel @Inject constructor(
+    private val repository: RecipeRepository
+) : BaseViewModel() {
+    var state by mutableStateOf(LikedScreenState())
+
+    init {
+        getFavorites()
+    }
+
+
+
+    private fun getFavorites() {
+        sendRequest(
+            request = {
+                repository.getEditorChoicePaging()
+            },
+            success = {
+                state = state.copy(
+                    isLoading = false,
+                    recipes = it,
+                    error = null
+                )
+
+                it.cachedIn(viewModelScope)
+
+            }
+        )
+
+    }
 }
