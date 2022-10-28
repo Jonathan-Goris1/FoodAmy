9d3eb4f6c0a3b9314d5fb4e858ef18a3a1c4aa30
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-28 15:50:57 -0400

    Added LikeScreenCard to LikedScreen.kt

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreen.kt
@@ -1,17 +1,24 @@
 package com.zybooks.foodamy.presentation.home_screens.liked_info
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 
 
 @Composable
-fun LikedScreen() {
-    Column {
-        Text(text = "This is the like screen")
+fun LikedScreen(
+    navController: NavController,
+    viewModel: LikeScreenViewModel = hiltViewModel()
+) {
 
-    }
+        Column {
+//            LikedScreenCard(Recipe =, likesData =) {
+
+            }
+
+
 }
 
 @Preview
