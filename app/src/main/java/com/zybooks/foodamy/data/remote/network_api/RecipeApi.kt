ee2301cfed26ccdeee4044d4ac4accec8f72ddfe
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-28 15:53:58 -0400

    changed return type for getRecipe function from dataResponse to recipeResponse.
    add a new api call for getting category with recipe

--- app/src/main/java/com/zybooks/foodamy/data/remote/network_api/RecipeApi.kt
+++ app/src/main/java/com/zybooks/foodamy/data/remote/network_api/RecipeApi.kt
@@ -1,6 +1,8 @@
 package com.zybooks.foodamy.data.remote.network_api
 
+import com.zybooks.foodamy.data.remote.response.CategoryPagingResponse
 import com.zybooks.foodamy.data.remote.response.DataResponse
+import com.zybooks.foodamy.data.remote.response.RecipeResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,10 +13,15 @@ interface RecipeApi {
     @GET("recipe/{id}")
     suspend fun getRecipe(
         @Path("recipeID") recipeID: Int,
-    ): DataResponse
+    ): RecipeResponse
 
     @GET("editor-choices")
     suspend fun getEditorsChoice(
         @Query("page") page: Int
     ): DataResponse
+
+    @GET("category-recipes")
+    suspend fun getCategoriesWithRecipes(
+        @Query("page") page: Int
+    ): CategoryPagingResponse
 }
