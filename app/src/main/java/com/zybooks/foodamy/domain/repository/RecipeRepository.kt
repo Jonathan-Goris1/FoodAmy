36e419bd0ee2bde735ffbc1565cb39fff10246a7
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-28 15:56:22 -0400

    Added getRecipeById function and removed GetRecipeInfo function

--- app/src/main/java/com/zybooks/foodamy/domain/repository/RecipeRepository.kt
+++ app/src/main/java/com/zybooks/foodamy/domain/repository/RecipeRepository.kt
@@ -1,17 +1,14 @@
 package com.zybooks.foodamy.domain.repository
 
 import androidx.paging.PagingData
-import com.zybooks.foodamy.data.remote.response.DataResponse
 import com.zybooks.foodamy.domain.model.Recipe
-import com.zybooks.foodamy.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface RecipeRepository {
 
-    suspend fun getRecipeInfo(
-        recipeID: Int
-    ): Resource<DataResponse>
 
     suspend fun getEditorChoicePaging(): Flow<PagingData<Recipe>>
+
+    suspend fun getRecipeById(recipeId: Int, onlyRemote: Boolean): Recipe
 
 }
