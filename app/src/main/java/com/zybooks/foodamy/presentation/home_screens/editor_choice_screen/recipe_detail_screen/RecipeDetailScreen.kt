607f7b3a80ba6fefede8c0d06d2ce43ad5689cd1
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-31 16:20:25 -0400

    Created scaffold and added RecipeDetailCard

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/editor_choice_screen/recipe_detail_screen/RecipeDetailScreen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/editor_choice_screen/recipe_detail_screen/RecipeDetailScreen.kt
@@ -1,4 +1,84 @@
 package com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.recipe_detail_screen
 
-class RecipeDetailScreen {
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.zybooks.foodamy.R
+import com.zybooks.foodamy.ui.components.RecipeDetailCard
+import com.zybooks.foodamy.ui.theme.DarkRed
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun RecipeDetailScreen(
+    recipeID: Int?,
+    viewModel: RecipeDetailViewModel = hiltViewModel(),
+    navController: NavController
+) {
+    val recipe = viewModel.state.recipe
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Recipe Name",  color = Color.White)
+
+                    }
+
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        androidx.compose.material.Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Go Back"
+                        )
+                    }
+                },
+                backgroundColor = DarkRed,
+
+                actions = {
+                    IconButton(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(Color.White)
+                            .size(30.dp, 30.dp),
+                        onClick = { /* doSomething() */ }) {
+                        Icon(
+                            modifier = Modifier.size(20.dp, 20.dp),
+                            painter = painterResource(id = R.drawable.ic_baseline_share_24),
+                            tint = DarkRed,
+                            contentDescription = "Share Recipe"
+                        )
+                    }
+
+
+                }
+            )
+        },
+
+        ) {
+        RecipeDetailCard(modifier = Modifier, recipe = recipe)
+    }
+
 }
