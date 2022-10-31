2930a98f8503d0e7c97a395fee1978dd7b42faa7
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-31 16:20:59 -0400

    Changed recipe parameter to be equal and an empty Recipe Model

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/editor_choice_screen/recipe_detail_screen/RecipeDetailState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/editor_choice_screen/recipe_detail_screen/RecipeDetailState.kt
@@ -1,12 +1,9 @@
 package com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.recipe_detail_screen
 
-import androidx.paging.PagingData
 import com.zybooks.foodamy.domain.model.Recipe
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
 
 data class RecipeDetailState(
     val isLoading: Boolean = false,
     val error: String? = null,
-    val recipe: Flow<PagingData<Recipe>> = emptyFlow()
+    val recipe: Recipe  = Recipe()
 )
