c03d66e1e73fdfba9a1bad021c71180bf86e4390
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-31 16:22:14 -0400

    Created function to request network api and store it into recipe state parameter.
    created a savedStateBundle to retrieve the recipe ID

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/editor_choice_screen/recipe_detail_screen/RecipeDetailViewModel.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/editor_choice_screen/recipe_detail_screen/RecipeDetailViewModel.kt
@@ -3,19 +3,41 @@ package com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.recip
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import com.zybooks.foodamy.domain.repository.RecipeRepository
 import com.zybooks.foodamy.presentation.base.BaseViewModel
+import com.zybooks.foodamy.util.DeviceConnection
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipeDetailViewModel @Inject constructor(
-    private val repository: RecipeRepository
+    private val repository: RecipeRepository,
+    savedStateHandle: SavedStateHandle,
+    private val deviceConnection: DeviceConnection
 ) : BaseViewModel() {
     var state by mutableStateOf(RecipeDetailState())
+    private val recipeID: Int = savedStateHandle.get<Int>("recipeId")!!
+
+    init {
+        recipeDetail()
+    }
 
 
-    fun recipeDetail(){
+    private fun recipeDetail(){
+        sendRequest(
+            request = {
+                repository.getRecipeById(recipeID, deviceConnection.isHasConnection() )
+            },
+            success = {
+                state = state.copy(
+                    isLoading = false,
+                    recipe = it,
+                    error = null
+                )
+
+            }
+        )
 
     }
 }
