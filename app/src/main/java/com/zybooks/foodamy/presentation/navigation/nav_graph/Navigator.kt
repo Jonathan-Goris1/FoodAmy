91ece1824048a7c29af1f5fe865a6392fb4c815d
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-31 16:18:27 -0400

    Removed composable for RecipeDetailScreen

--- app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/Navigator.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/Navigator.kt
@@ -1,11 +1,9 @@
 package com.zybooks.foodamy.presentation.navigation.nav_graph
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.AppScaffold
 import com.zybooks.foodamy.presentation.navigation.AUTHENTICATION_ROUTE
 import com.zybooks.foodamy.presentation.navigation.Screen
@@ -27,16 +25,7 @@ fun Navigator(){
             route = Screen.AppScaffold.route,
             content = { AppScaffold(navController = navBarController) }
         )
-        composable(
-            route = "${Screen.RecipeDetail.route}?recipeId={recipeId}",
-            arguments = listOf(
-                navArgument(name = "recipeId") {
-                    type = NavType.IntType
-                }
-            )
-        ){
-            //TODO Make RecipeDetailsScreen and add it here
-        }
+
 
 
 
