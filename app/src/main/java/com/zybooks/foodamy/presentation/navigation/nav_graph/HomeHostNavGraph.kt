d98e84caa0ab8487c1769beab4e3a9f6e26b3a20
Jonathan Goris <jonathangoris123@gmail.com>
2022-10-31 16:17:46 -0400

    Added RecipeDetailScreen composable for navigation

--- app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/HomeHostNavGraph.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/HomeHostNavGraph.kt
@@ -3,13 +3,17 @@ package com.zybooks.foodamy.presentation.navigation.nav_graph
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.RecipeHomeScreen
+import com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.recipe_detail_screen.RecipeDetailScreen
 import com.zybooks.foodamy.presentation.home_screens.liked_info.LikedScreen
 import com.zybooks.foodamy.presentation.home_screens.menu_info.MenuScreen
 import com.zybooks.foodamy.presentation.home_screens.users_info.UsersScreen
 import com.zybooks.foodamy.presentation.navigation.BottomNavItem
+import com.zybooks.foodamy.presentation.navigation.Screen
 
 @Composable
 
@@ -19,13 +23,14 @@ fun HomeHostNavGraph(navController: NavController){
         navController = navController as NavHostController,
         startDestination = BottomNavItem.Recipe.screen_route,
 
+
     ) {
 
         composable(BottomNavItem.Recipe.screen_route) {
             RecipeHomeScreen(navController)
         }
         composable(BottomNavItem.Liked.screen_route) {
-            LikedScreen()
+            LikedScreen(navController)
         }
         composable(BottomNavItem.Users.screen_route) {
             UsersScreen()
@@ -33,5 +38,19 @@ fun HomeHostNavGraph(navController: NavController){
         composable(BottomNavItem.Menu.screen_route) {
             MenuScreen()
         }
+
+        composable(
+            route = "${Screen.RecipeDetail.route}?recipeId={recipeId}",
+            arguments = listOf(
+                navArgument(name = "recipeId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ){ backStackEntry ->
+            //TODO Make RecipeDetailsScreen and add it here
+            RecipeDetailScreen(recipeID = backStackEntry.arguments?.getInt("recipeId"), navController = navController)
+        }
+
     }
 }
