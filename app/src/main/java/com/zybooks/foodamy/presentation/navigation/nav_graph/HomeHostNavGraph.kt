9e4a8541ea08979b9b7d6f76c2417acd85d0989e
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-03 15:18:37 -0400

    Removed detail screen composable and added detailNavGraph function

--- app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/HomeHostNavGraph.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/navigation/nav_graph/HomeHostNavGraph.kt
@@ -3,20 +3,15 @@ package com.zybooks.foodamy.presentation.navigation.nav_graph
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.RecipeHomeScreen
-import com.zybooks.foodamy.presentation.home_screens.editor_choice_screen.recipe_detail_screen.RecipeDetailScreen
 import com.zybooks.foodamy.presentation.home_screens.liked_info.LikedScreen
 import com.zybooks.foodamy.presentation.home_screens.menu_info.MenuScreen
 import com.zybooks.foodamy.presentation.home_screens.users_info.UsersScreen
 import com.zybooks.foodamy.presentation.navigation.BottomNavItem
-import com.zybooks.foodamy.presentation.navigation.Screen
 
 @Composable
-
 fun HomeHostNavGraph(navController: NavController){
 
     NavHost(
@@ -38,19 +33,7 @@ fun HomeHostNavGraph(navController: NavController){
         composable(BottomNavItem.Menu.screen_route) {
             MenuScreen()
         }
-
-        composable(
-            route = "${Screen.RecipeDetail.route}?recipeId={recipeId}",
-            arguments = listOf(
-                navArgument(name = "recipeId") {
-                    type = NavType.IntType
-                    defaultValue = 0
-                }
-            )
-        ){ backStackEntry ->
-            //TODO Make RecipeDetailsScreen and add it here
-            RecipeDetailScreen(recipeID = backStackEntry.arguments?.getInt("recipeId"), navController = navController)
-        }
+        detailNavGraph(navController)
 
     }
 }
