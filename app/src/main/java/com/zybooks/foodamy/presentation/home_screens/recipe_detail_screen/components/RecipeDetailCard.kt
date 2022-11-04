ff891b0bc43017623b632f58ed7d5e68da9d7b1b
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-04 17:51:30 -0400

    Removed modifier parameter from LikeScreenCard function

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/components/RecipeDetailCard.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/components/RecipeDetailCard.kt
@@ -1,17 +1,17 @@
 package com.zybooks.foodamy.presentation.home_screens.recipe_detail_screen.components
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.zybooks.foodamy.domain.model.Recipe
 
 @Composable
 fun RecipeDetailCard(
-    modifier: Modifier,
     recipe: Recipe
 ) {
     
-    RecipeDetailProfileSection(modifier = modifier, recipe = recipe )
+    RecipeDetailProfileSection( recipe = recipe )
     RecipeDetailStepsSection(recipe = recipe)
+    RecipeDetailCommentSection(recipe = recipe)
+    RecipeDetailSimilarRecipeSection(recipe = recipe)
 
 }
 
