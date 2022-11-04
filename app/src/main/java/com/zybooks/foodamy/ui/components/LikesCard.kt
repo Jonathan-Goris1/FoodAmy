5a2b73ca8708e83aaee7388fc13cc135418f3c56
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-04 17:49:19 -0400

    Improved LikesCard.kt

--- app/src/main/java/com/zybooks/foodamy/ui/components/LikesCard.kt
+++ app/src/main/java/com/zybooks/foodamy/ui/components/LikesCard.kt
@@ -1,43 +1,61 @@
 package com.zybooks.foodamy.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.zybooks.foodamy.domain.model.Recipe
 
 @Composable
 fun LikesCard(
-    modifier: Modifier,
     Recipe: Recipe
 ){
 
-    Card() {
-        Column() {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Column {
 
             Image(
                 modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .padding(8.dp)
+                    .fillMaxWidth(0.5f)
+                    .height(300.dp)
                     .clip(shape = RectangleShape),
                 painter = rememberAsyncImagePainter(Recipe.category.image?.url),
+                contentScale = ContentScale.Crop,
                 contentDescription = null,
             )
             
-            Text(modifier = modifier.padding(8.dp),
-                text = Recipe.title)
+            Text(modifier = Modifier.padding(8.dp),
+                text = Recipe.title,
+                fontSize = 12.sp
+            )
 
-            Text(text = "${Recipe.commentCount} Comments ${Recipe.likeCount} Taste")
+            Box(modifier = Modifier
+                .background(color = Color.LightGray)
+                .fillMaxWidth(0.5F)){
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(8.dp),
+                    text = "${Recipe.commentCount} Comments ${Recipe.likeCount} Taste"
+                )
+            }
+
+
 
         }
 
