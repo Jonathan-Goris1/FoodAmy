a2d850d5a5f87d3161dffbcfea42611d834dbc19
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-04 17:48:44 -0400

    Added contentScale attribute to Image for crop

--- app/src/main/java/com/zybooks/foodamy/ui/components/EditorsChoiceRecipeCard.kt
+++ app/src/main/java/com/zybooks/foodamy/ui/components/EditorsChoiceRecipeCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,11 +50,12 @@ fun EditorsChoiceRecipeCard(
                         .padding(8.dp)
                         .clip(shape = CircleShape),
                     painter = rememberAsyncImagePainter(Recipe.user.image?.url),
+
                     contentDescription = null,
                     )
 
                 Column {
-                    Text(text = Recipe.user.name ?: "")
+                    Text(text = Recipe.user.name)
 
                     Spacer(modifier = modifier.size(4.dp))
 
@@ -93,6 +95,7 @@ fun EditorsChoiceRecipeCard(
                         .padding(8.dp)
                         .clip(shape = RoundedCornerShape(4.dp)),
                     painter = rememberAsyncImagePainter(Recipe.category.image?.url),
+                    contentScale = ContentScale.Crop,
                     contentDescription = null
                 )
 
