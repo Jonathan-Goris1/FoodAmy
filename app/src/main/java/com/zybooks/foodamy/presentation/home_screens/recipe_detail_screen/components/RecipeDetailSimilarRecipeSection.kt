a156685795428c6a1d0933735f28ff54346dae9b
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-04 17:53:23 -0400

    Created a similar recipe section file to create the section for similar recipes in recipe detail

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/components/RecipeDetailSimilarRecipeSection.kt
@@ -0,0 +1,33 @@
+package com.zybooks.foodamy.presentation.home_screens.recipe_detail_screen.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.zybooks.foodamy.domain.model.Recipe
+import com.zybooks.foodamy.ui.components.LikesCard
+
+@Composable
+fun RecipeDetailSimilarRecipeSection(
+    recipe: Recipe
+) {
+    Column() {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "Similar Recipe"
+        )
+
+        Divider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Color.LightGray
+        )
+
+    }
+    LikesCard(Recipe = recipe)
+}
