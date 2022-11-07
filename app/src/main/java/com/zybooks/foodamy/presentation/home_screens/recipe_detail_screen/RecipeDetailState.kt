631ab32293a3cea028a5b72d636003a1f2fc0c2c
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-07 15:07:49 -0500

    Added new parameter for comments

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/RecipeDetailState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/RecipeDetailState.kt
@@ -1,9 +1,14 @@
 package com.zybooks.foodamy.presentation.home_screens.recipe_detail_screen
 
+import androidx.paging.PagingData
+import com.zybooks.foodamy.domain.model.Comment
 import com.zybooks.foodamy.domain.model.Recipe
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class RecipeDetailState(
     val isLoading: Boolean = false,
     val error: String? = null,
-    val recipe: Recipe  = Recipe()
+    val recipe: Recipe  = Recipe(),
+    val comment: Comment = Comment()
 )
