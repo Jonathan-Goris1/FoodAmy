17dc451a88156176fbed178b601724c055171c31
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-07 15:03:53 -0500

    added new inserts and select query to insert and retrieve from the comments database

--- app/src/main/java/com/zybooks/foodamy/data/local/dao/RecipeDao.kt
+++ app/src/main/java/com/zybooks/foodamy/data/local/dao/RecipeDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.zybooks.foodamy.data.local.local_dto.CommentDb
 import com.zybooks.foodamy.data.local.local_dto.RecipeDb
 
 @Dao
@@ -13,11 +14,32 @@ interface RecipeDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipes(recipes: List<RecipeDb>)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComments(comments: List<CommentDb>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComment(comment: CommentDb)
+
     @Query("select * from recipes where is_editor_choice = 1 order by id desc")
     fun getEditorChoicesPaging(): PagingSource<Int, RecipeDb>
 
     @Query("select * from recipes where is_editor_choice =1")
     suspend fun getEditorChoices(): List<RecipeDb>
+
+    @Query("select * from comments where recipe_id =:recipeId order by id desc")
+    fun getRecipeCommentsPaging(recipeId: Int): PagingSource<Int, CommentDb>
+
+    @Query("select * from comments where recipe_id =:recipeId")
+    suspend fun getRecipeComments(recipeId: Int): List<CommentDb>
+
+    @Query("delete from comments where id =:commentId")
+    suspend fun deleteComment(commentId: Int)
+
+    @Query("select * from comments where recipe_id =:recipeId order by id desc limit 1")
+    suspend fun getFirstRecipeComments(recipeId: Int): CommentDb
+
+    @Query("delete from comments")
+    suspend fun deleteComments()
 
     @Query("DELETE FROM recipes")
     suspend fun clearRecipeListing()
