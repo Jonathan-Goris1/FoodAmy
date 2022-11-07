cbc7539fced6cc9e09932a00d705fcae991a471a
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-07 15:04:47 -0500

    added a new parameter comments for the RecipeDetailCommentSection.kt added a new composable RecipeDetailDirectionSection.kt

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/components/RecipeDetailCard.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/recipe_detail_screen/components/RecipeDetailCard.kt
@@ -1,16 +1,20 @@
 package com.zybooks.foodamy.presentation.home_screens.recipe_detail_screen.components
 
 import androidx.compose.runtime.Composable
+import com.zybooks.foodamy.domain.model.Comment
 import com.zybooks.foodamy.domain.model.Recipe
 
 @Composable
 fun RecipeDetailCard(
-    recipe: Recipe
+    recipe: Recipe,
+    comment: Comment
+
 ) {
     
     RecipeDetailProfileSection( recipe = recipe )
-    RecipeDetailStepsSection(recipe = recipe)
-    RecipeDetailCommentSection(recipe = recipe)
+    RecipeDetailIngredientsSection(recipe = recipe)
+    RecipeDetailDirectionSection(recipe = recipe)
+    RecipeDetailCommentSection(comment = comment)
     RecipeDetailSimilarRecipeSection(recipe = recipe)
 
 }
