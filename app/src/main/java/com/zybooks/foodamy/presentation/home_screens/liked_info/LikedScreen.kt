cf85fa0130a7adf76a405d0e43bb21d24b468aa7
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-08 16:15:27 -0500

    added the LikeScreenCard.kt composable to the lazy column

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreen.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreen.kt
@@ -1,10 +1,15 @@
 package com.zybooks.foodamy.presentation.home_screens.liked_info
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.zybooks.foodamy.ui.components.LikedScreenCard
 
 
 @Composable
@@ -12,11 +17,19 @@ fun LikedScreen(
     navController: NavController,
     viewModel: LikeScreenViewModel = hiltViewModel()
 ) {
+    val categoryItems = viewModel.state.recipes.collectAsLazyPagingItems()
 
-        Column {
-//            LikedScreenCard(Recipe =, likesData =) {
+    LazyColumn(
+        modifier = Modifier.padding(0.dp,0.dp, 0.dp,48.dp)
+    ) {
+        items(categoryItems.itemCount) { index ->
+            categoryItems[index]?.recipes?.let {
+                LikedScreenCard(category = categoryItems[index], likesData = it) {
 
+                }
             }
+        }
+    }
 
 
 }
