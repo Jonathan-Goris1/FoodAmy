9cac6e56ccc5a250e87a98f29c087f625d9202e3
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-08 16:15:52 -0500

    Changes state type to for recipes to category

--- app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreenState.kt
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/liked_info/LikedScreenState.kt
@@ -1,12 +1,12 @@
 package com.zybooks.foodamy.presentation.home_screens.liked_info
 
 import androidx.paging.PagingData
-import com.zybooks.foodamy.domain.model.Recipe
+import com.zybooks.foodamy.domain.model.Category
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 
 data class LikedScreenState (
     val isLoading: Boolean = false,
     val error: String? = null,
-    val recipes: Flow<PagingData<Recipe>> = emptyFlow()
+    val recipes: Flow<PagingData<Category>> = emptyFlow()
 )
