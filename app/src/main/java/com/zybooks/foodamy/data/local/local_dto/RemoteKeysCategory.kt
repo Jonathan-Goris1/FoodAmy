8de4e2cf2b709e2b4c1d10434c5e833ad807d8d1
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-08 16:21:21 -0500

    created a new db entity for RemoteKeysCategory.kt

---
+++ app/src/main/java/com/zybooks/foodamy/data/local/local_dto/RemoteKeysCategory.kt
@@ -0,0 +1,13 @@
+package com.zybooks.foodamy.data.local.local_dto
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.zybooks.foodamy.data.utils.RemoteKey
+
+@Entity(tableName = "remote_keys_category")
+data class RemoteKeysCategory(
+    @PrimaryKey(autoGenerate = false)
+    override val id: Int,
+    override val prev: Int?,
+    override val next: Int?
+) : RemoteKey
