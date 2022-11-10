3c72044ea5b5e6c0964621c27c18916cbe0ed21c
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-10 15:50:53 -0500

    Added the bind function for the user repository

--- app/src/main/java/com/zybooks/foodamy/di/RepositoryModule.kt
+++ app/src/main/java/com/zybooks/foodamy/di/RepositoryModule.kt
@@ -4,10 +4,13 @@ import com.zybooks.foodamy.data.local.dao.RemoteKeysDao
 import com.zybooks.foodamy.data.local.database.AppDatabase
 import com.zybooks.foodamy.data.remote.network_api.AuthApi
 import com.zybooks.foodamy.data.remote.network_api.RecipeApi
+import com.zybooks.foodamy.data.remote.network_api.UserApi
 import com.zybooks.foodamy.data.repository.AuthRepositoryImpl
 import com.zybooks.foodamy.data.repository.RecipeRepositoryImpl
+import com.zybooks.foodamy.data.repository.UserRepositoryImpl
 import com.zybooks.foodamy.domain.repository.AuthRepository
 import com.zybooks.foodamy.domain.repository.RecipeRepository
+import com.zybooks.foodamy.domain.repository.UserRepository
 import com.zybooks.foodamy.domain.utils.DataStoreManager
 import dagger.Module
 import dagger.Provides
@@ -36,5 +39,13 @@ object RepositoryModule {
         remoteKeysDao: RemoteKeysDao
     ): RecipeRepository{
         return RecipeRepositoryImpl(api, database.recipeDao, remoteKeysDao)
+    }
+
+    @Provides
+    @Singleton
+    fun bindUserRepository(
+        api: UserApi
+    ): UserRepository{
+        return UserRepositoryImpl(api)
     }
 }
