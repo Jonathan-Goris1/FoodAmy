fdd89a4d02b8f9e4a1b3262f6a6f2eae8d1d8ef1
Jonathan Goris <jonathangoris123@gmail.com>
2022-11-10 15:53:12 -0500

    Created a separate Composable called UserScreenLikesSection.kt to show the Likes section of the screen

---
+++ app/src/main/java/com/zybooks/foodamy/presentation/home_screens/users_info/components/UserScreenLikesSection.kt
@@ -0,0 +1,50 @@
+package com.zybooks.foodamy.presentation.home_screens.users_info.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zybooks.foodamy.domain.model.Recipe
+import com.zybooks.foodamy.ui.components.LikesCard
+
+@Composable
+fun UserScreenLikesSection(
+    Likes: List<Recipe>
+){
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+
+        Text(
+            modifier = Modifier
+                .padding(8.dp),
+            text = "Likes",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Divider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 8.dp),
+            thickness = 1.dp,
+            color = Color.LightGray
+        )
+
+        LazyRow {
+            items(Likes) { recipe ->
+                LikesCard(recipe = recipe)
+
+            }
+        }
+    }
+}
